ddbd1b53834a6ba18f2df53a2e22087fa969dacf
Deekshitha Reddy Manda <deekshareddy3508@gmail.com>
2026-06-15 13:20:37 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        `${import.meta.env.VITE_API_URL}/api/auth/login`,
         {
           email,
           password,
